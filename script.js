60e4874788b5d4ccfd2df561fa853cf429f05d94
kubrabuzlu <kubraa.buzlu@gmail.com>
2023-04-12 22:57:17 +0300

    Efektler eklendi

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const flexItems = document.querySelectorAll('.flex-item');
 var button = document.querySelector('#change-color');
+var button2 = document.querySelector('.btn-6d');
 button.addEventListener('click', function(){
     flexItems.forEach((item) => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -9,3 +10,6 @@ button.addEventListener('click', function(){
     });
 });
 
+button2.addEventListener('click', function(){
+    window.open('https://www.python.org/', '_blank');
+});
